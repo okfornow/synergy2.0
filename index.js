1201ec67ee25d1f59f12e7a0ae514ce0dcb9bb90
okfornow <tryingtobejunior.js@gmail.com>
2022-09-05 11:13:55 -0400

    add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+let obj = {
+    age: 30,
+    name: "ded"
+}
+
+function checkObject(obj) {
+    if (Array.isArray(obj)) {
+        console.log("its array");
+    } else if (obj == 0) {
+        console.log("its empty")
+    } else if (typeof obj === "object") {
+        console.log(obj);
+    }
+}
+
+checkObject(obj);
